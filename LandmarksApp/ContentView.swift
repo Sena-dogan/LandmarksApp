76d20aeeb1c6b04b1881b1d7a89b097394bb268b
Zeynep Sena Dogan <zeynepsena2002.d@gmail.com>
2024-05-18 17:48:58 +0300

    Initial Commit

---
+++ LandmarksApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LandmarksApp
+//
+//  Created by Zeynep Sena Doğan on 18.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Zeynep Sena Doğan on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
